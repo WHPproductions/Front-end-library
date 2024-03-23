c45575164ec7121b75ec581afeab555598fbbbc3
Winata Hadi Pratama <winatahadipratama@gmail.com>
2024-03-23 12:50:48 +0700

    Initialize some more elements and some stylings to get the basic idea of what the app would look like

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+class Book {
+    constructor (title, author, year, isComplete) {
+        this.id = +new Date()
+        this.title = title
+        this.author = author
+        this.year = year 
+        this.isComplete = isComplete
+    }
+}
+
 const library = []
