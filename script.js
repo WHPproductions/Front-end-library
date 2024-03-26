dd76bf688da12de83ae40a7ece77bef9df5af4d5
Winata Hadi Pratama <winatahadipratama@gmail.com>
2024-03-26 16:25:42 +0700

    Added some comments and add more stylings, also add a modal popup function

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ class Book {
 }
 
 const library = []
+
+// Showing and closing modals
+
+const showFormModalButton = document.querySelector('.show-simpan-buku')
+const showSearchModalButton = document.querySelector('.show-cari-buku')
+const formModal = document.getElementById('simpan-buku')
+const searchModal = document.getElementById('cari-buku')
+const hideFormModalButton = document.querySelector('.hide-simpan-buku')
+const hideSearchModalButton = document.querySelector('.hide-cari-buku')
+
+showFormModalButton.addEventListener('click', () => {
+    formModal.showModal()
+})
+
+showSearchModalButton.addEventListener('click', () => {
+    searchModal.showModal()
+})
+
+hideFormModalButton.addEventListener('click', ()    => {
+    formModal.close()
+})
+
+hideSearchModalButton.addEventListener('click', ()    => {
+    searchModal.close()
+})
